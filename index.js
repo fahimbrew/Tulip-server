fc7a8bb664f38365a8eeb6d5bf0ec18a0188fa16
fahimbrew <iftiaqhossain@gmail.com>
2025-03-07 00:46:26 +0600

    individual detail page server has been created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 4000;
 
@@ -33,6 +33,13 @@ async function run() {
     app.get("/running-campaign",async(req,res)=>{
         const campaign = campaignCollection.find().limit(6);
         const result = await campaign.toArray();
+        res.send(result);
+    })
+
+    app.get("/running-campaign/:id",async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id:ObjectId(id)};
+        const result = await find(query);
         res.send(result);
     })
 
